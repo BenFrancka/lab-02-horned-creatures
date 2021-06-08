0cfb3bb40f09a56c3c49349d0a6f92c7704b43a2
BenFrancka <benfrancka@gmail.com>
2021-06-08 14:21:31 -0700

    added Header

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+
+                <hi>
+                    "Are you horny, baby?"
+                    <img src= "powers.png" width= "100" height= "100"/>
+                </hi>
+
+                
+            </div>
+        )
+    }
+}
